cd47fb74040bc180e434b2dd9a0ee49ff0a145dd
Ido Galili <ido.galili@zoho.com>
2018-12-09 22:09:16 +0200

    add skills logic

---
+++ main.js
@@ -0,0 +1,68 @@
+let skillsArr = [
+    {
+        title: "JavaScript",
+        icon: "devicon-javascript-plain colored"
+    },
+    {
+        title: "CSS3",
+        icon: "devicon-css3-plain colored"
+    },
+    {
+        title: "Bootstrap",
+        icon: "devicon-bootstrap-plain colored"
+    },
+    {
+        title: "Git",
+        icon: "devicon-git-plain colored"
+    },
+    {
+        title: "ExpressJS",
+        icon: "devicon-express-original-wordmark colored"
+    },
+    {
+        title: "NodeJS",
+        icon: "devicon-nodejs-plain-wordmark colored"
+    },
+    {
+        title: "HTML5",
+        icon: "devicon-html5-plain colored"
+    },
+    {
+        title: "MongoDB",
+        icon: "devicon-mongodb-plain colored"
+    },
+]
+
+function populateSkills(){
+    sortByTitle(skillsArr)
+
+    skillsArr.forEach(skill => {
+        let str = `<div class="card bg-light text-center border-dark mr-4 mb-4" style="width: 11rem;">
+                    <div class="card-body">
+                        <i class="${skill.icon}"></i>
+                    </div>
+                    <div class="card-footer border-dark">${skill.title}</div>
+                </div>`
+        console.log(str)
+        showSkills.innerHTML += str
+    })
+}
+
+function sortByTitle(objectsArr){
+    objectsArr.sort(function(obj1, obj2){
+        if (obj1.title < obj2.title){
+            return -1
+        } else if(obj1.title === obj2.title){
+            return 0
+        } else {
+            return 1
+        }
+    })
+}
+
+populateSkills()
+
+
+// function toPosition(divid){
+//     $('html, body').animate({scrollTop:$(divid).position().top - 50 }, 'slow');
+// }
