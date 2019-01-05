f4623c280673494d40fce26dd402eae73e2bab2d
Ido Galili <ido.galili@zoho.com>
2019-01-05 14:11:57 +0200

    Switched to Flexbox

--- data.js
+++ data.js
@@ -38,25 +38,30 @@ let projectsArr = [
         title: "Color Blocks",
         image: "./images/projects/color-blocks-preview.png",
         link: "./projects/Color Blocks/colorBlocks.html",
+        skills: ["JavaScript", "CSS3", "Bootstrap", "HTML5"]
     },
     {
         title: "Task Board",
         image: "./images/projects/tasks-board-preview.png",
         link: "./projects/Task Board/index.html",
+        skills: ["JavaScript", "CSS3", "Bootstrap", "HTML5"]
     },
     {
         title: "Unit Calculator",
         image: "./images/projects/unit-calculator-preview.png",
         link: "./projects/Unit Calculator/index.html",
+        skills: ["JavaScript", "CSS3", "Bootstrap", "HTML5"]
     },
     {
         title: "User Table",
         image: "./images/projects/user-table-preview.png",
         link: "./projects/User Table/home.html",
+        skills: ["JavaScript", "CSS3", "Bootstrap", "HTML5"]
     },
     {
         title: "Draw Circles",
         image: "./images/projects/circles-preview.png",
         link: "./projects/Circles/index.html",
+        skills: ["JavaScript", "CSS3", "Bootstrap", "HTML5"]
     }
 ]
--- main.js
+++ main.js
@@ -1,43 +1,71 @@
-'use strict'
-
-function populateSkills(){
+function populateSkills() {
     sortByTitle(skillsArr)
     let str = ``
 
     skillsArr.forEach(skill => {
-        str += `<div class="skill-card card bg-light text-center border-dark mr-4 mb-4">`
+        str += `<div class="card">`
         str += `<div class="card-body">`
         str += `<i class="${skill.icon}"></i>`
         str += `</div>`
-        str += `<div class="card-footer border-dark">${skill.title}</div>`
+        str += `<div class="card-footer">${skill.title}</div>`
         str += `</div>`
     })
 
-    document.getElementById("showSkills").innerHTML += str
+    document.querySelector("#show-skills").innerHTML += str
 }
 
-function populateProjects(){
+function populateProjects() {
     sortByTitle(projectsArr)
-    let str = ``
-    
-    projectsArr.forEach(project => {
-        str += `<div class="project-card m-4" onclick="window.open('${project.link}')">`
-        str += `<img class="project-image" src="${project.image}">`
-        str += `<div class="overlay-middle">`
-        str += `<i class="far fa-eye fa-4x"></i>`
-        str += `<h3>${project.title}</h3>`
-        str += `</div>`
-        str += `</div>`
-    })
 
-    document.getElementById("showProjects").innerHTML += str
+    projectsArr.forEach(project => {
+        let newCard = document.createElement("div")
+        let cardBody = document.createElement("div")
+        let cardFooter = document.createElement("div")
+
+        newCard.classList.add(`card`)
+        newCard.onclick = () => {
+            window.open(project.link)
+        }
+        cardBody.classList.add('card-body')
+        cardFooter.classList.add(`card-footer`)
+
+        cardBody.innerHTML = createCardBody(project)
+        cardFooter.innerHTML = createCardFooter(project)
+        newCard.appendChild(cardBody)
+        newCard.appendChild(cardFooter)
+        document.querySelector("#show-projects").appendChild(newCard)
+    })
 }
 
-function sortByTitle(objectsArr){
-    objectsArr.sort(function(obj1, obj2){
-        if (obj1.title < obj2.title){
+function createCardBody(project) {
+    let str = ``
+
+    str += `<img class="card-image" src="${project.image}">`
+    str += `<div class="overlay">`
+    str += `<svg class="far fa-eye fa-4x"></svg>`
+    str += `<h3>${project.title}</h3>`
+    str += `</div>`
+
+    return str;
+}
+
+function createCardFooter(project) {
+    let str = ``
+    project.skills.forEach(skill => {
+        skillsArr.forEach(skillObj => {
+            if (skillObj.title === skill) {
+                str += `<i class="${skillObj.icon}"></i>`
+            }
+        })
+    })
+    return str;
+}
+
+function sortByTitle(objectsArr) {
+    objectsArr.sort(function (obj1, obj2) {
+        if (obj1.title < obj2.title) {
             return -1
-        } else if(obj1.title === obj2.title){
+        } else if (obj1.title === obj2.title) {
             return 0
         } else {
             return 1
@@ -45,13 +73,12 @@ function sortByTitle(objectsArr){
     })
 }
 
-function initPage(){
+function initPage() {
     populateSkills()
     populateProjects()
 }
 
 window.onload = () => {
+    window.scroll(0, 60)
     initPage()
 }
-
-
