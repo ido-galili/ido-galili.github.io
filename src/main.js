e7cd07eaaaffb62fc40e7beeda2559cee21e4de5
Ido Galili <ido.galili@zoho.com>
2019-02-10 15:28:47 +0200

    add resume in site
    added hebrew and english resume in site, scale skills svg

--- src/main.js
+++ src/main.js
@@ -1,28 +1,53 @@
 let currentCarouselItem = 0
 
-function populateSkills() {
+function initPage() {
+    setSkills()
+    setShowcase()
+    setResume()
+}
+
+window.onload = () => {
+    // window.scroll(0, 60)
+    initPage()
+    document.getElementById('english-btn').onclick = () => {
+        let english = document.getElementById('english-resume')
+        if (!english.classList.contains('resume-active')) {
+            document.getElementById('hebrew-resume').classList.remove('resume-active')
+            document.getElementById('english-resume').classList.add('resume-active')
+        }
+
+    }
+    document.getElementById('hebrew-btn').onclick = () => {
+        let hebrew = document.getElementById('hebrew-resume')
+        if (!hebrew.classList.contains('resume-active')) {
+            document.getElementById('english-resume').classList.remove('resume-active')
+            document.getElementById('hebrew-resume').classList.add('resume-active')
+
+        }
+    }
+}
+
+function setSkills() {
     let str = ``
 
     skillsArr.forEach(skill => {
         str += `<div class="card">`
-        str += `<div class="card-body">`
-        // str += `<i class="${skill.icon}"></i>`
-        str += skill.icon
-        str += `</div>`
-        str += `<div class="card-footer">${skill.title}</div>`
+        // str += `<div class="card-body">`
+        str += `<img src=${skill.icon}>`
+        // str += `</div>`
+        // str += `<div class="card-footer">${skill.title}</div>`
         str += `</div>`
     })
 
     document.querySelector("#skills").innerHTML += str
 }
 
-function populateShowcase(){
+function setShowcase() {
     let str = ``
 
-    sortByTitle(projectsArr)
     let random = Math.floor(Math.random() * projectsArr.length)
 
-    projectsArr.forEach((project,i) => {
+    projectsArr.forEach((project, i) => {
         str = ``
 
         let newItem = document.createElement('div')
@@ -32,15 +57,20 @@ function populateShowcase(){
             window.open(project.link)
         }
 
-        if(random === i){
+        if (random === i) {
             currentCarouselItem = i
-            newItem.classList.add('active') 
+            newItem.classList.add('active')
         }
 
         str += `<img src="${project.image}">`
         str += `<div class="overlay">`
         str += `<svg class="far fa-eye fa-4x"></svg>`
         str += `<h3>${project.title}</h3>`
+        // str += `<div class="overlay-icons">`
+        // str += `<img src = "./src/icons/mongodb-original.svg">`
+        // str += `<img src = "./src/icons/mongodb-original.svg">`
+        // str += `<img src = "./src/icons/mongodb-original.svg">`
+        // str += `</div>`
         str += `</div>`
 
         newItem.innerHTML = str
@@ -48,23 +78,23 @@ function populateShowcase(){
     })
 }
 
-function carouselMoveRight(){
+function carouselMoveRight() {
     document.getElementsByClassName('active')[0].classList.remove("active")
-    if(currentCarouselItem === projectsArr.length-1){
+    if (currentCarouselItem === projectsArr.length - 1) {
         document.getElementById('0').classList.add("active")
         currentCarouselItem = 0
     } else {
-        document.getElementById('' + (++currentCarouselItem) ).classList.add("active")
+        document.getElementById('' + (++currentCarouselItem)).classList.add("active")
     }
 }
 
-function carouselMoveLeft(){
+function carouselMoveLeft() {
     document.getElementsByClassName('active')[0].classList.remove("active")
-    if(currentCarouselItem === 0){
-        document.getElementById('' + projectsArr.length-1).classList.add("active")
-        currentCarouselItem = projectsArr.length-1
+    if (currentCarouselItem === 0) {
+        document.getElementById('' + projectsArr.length - 1).classList.add("active")
+        currentCarouselItem = projectsArr.length - 1
     } else {
-        document.getElementById('' + (--currentCarouselItem) ).classList.add("active")
+        document.getElementById('' + (--currentCarouselItem)).classList.add("active")
     }
 }
 
@@ -80,12 +110,84 @@ function sortByTitle(objectsArr) {
     })
 }
 
-function initPage() {
-    populateSkills()
-    populateShowcase()
+function setResume() {
+    setEnglishResume()
+    setHebrewResume()
 }
 
-window.onload = () => {
-    // window.scroll(0, 60)
-    initPage()
+function setEnglishResume() {
+    let str = ``
+
+    str += `<p class="resume-title">Education</p>`
+
+    str += getEducation(englishResumeObj)
+
+    document.getElementById(`education-en`).innerHTML += str
+
+    str = ``
+
+    str += `<p class="resume-title">Work Experience</p>`
+
+    str += getExperience(englishResumeObj)
+
+    document.getElementById(`experience-en`).innerHTML += str
+}
+
+function setHebrewResume() {
+    let str = ``
+
+    str += `<p class="resume-title">השכלה</p>`
+
+    str += getEducation(hebrewResumeObj)
+
+    document.getElementById(`education-he`).innerHTML += str
+
+    str = ``
+
+    str += `<p class="resume-title">ניסיון תעסוקתי</p>`
+
+    str += getExperience(hebrewResumeObj)
+
+    document.getElementById(`experience-he`).innerHTML += str
+}
+
+function getEducation(obj) {
+    let str = ``
+
+    obj.education.forEach(block => {
+        str += `<div class="resume-block">`
+        str += `<p class="resume-year">${block.years}</p>`
+        str += `<div class="resume-job">`
+        str += `<p>${block.title}</p>`
+        str += `<ul>`
+        block.description.forEach(desc => {
+            str += `<li>${desc}</li>`
+        })
+
+        str += `</ul>`
+        str += `</div>`
+        str += `</div>`
+    })
+
+    return str
+}
+
+function getExperience(obj) {
+    let str = ``
+
+    obj.experience.forEach(block => {
+        str += `<div class="resume-block">`
+        str += `<p class="resume-year">${block.years}</p>`
+        str += `<div class="resume-job">`
+        str += `<p>${block.title}</p>`
+        str += `<ul>`
+        block.description.forEach(desc => {
+            str += `<li>${desc}</li>`
+        })
+        str += `</ul>`
+        str += `</div>`
+        str += `</div>`
+    })
+
+    return str
 }
